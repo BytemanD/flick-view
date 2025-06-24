cd2eb07d66d5f5071b585d3cf8a3b4ac58da290a
BytemanD <byteman.d@outlook.com>
2025-06-24 22:17:21 +0800

    feat: 修改图标
    Signed-off-by: BytemanD <byteman.d@outlook.com>

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,18 +3,24 @@
         <v-app>
             <v-navigation-drawer :rail="navigation.mini">
                 <v-list>
-                    <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-                    subtitle="sandra_a88@gmailcom" title="Sandra Adams"></v-list-item>
+                    <v-list-item subtitle="guest@gmailcom" title="Guest">
+                        <template v-slot:prepend>
+                            <v-avatar image="@/assets/logo.svg" rounded="0"></v-avatar>
+                        </template>
+                    </v-list-item>
                 </v-list>
 
                 <v-divider></v-divider>
-                
+
                 <v-list density="compact" nav>
-                    <v-list-item title="Python Manager" value="myfiles" prepend-icon="mdi-home">
+                    <v-list-item title="Python Manager" value="pythonManager">
+                        <template v-slot:prepend>
+                            <v-img :width="22" src="/python.svg" class="mr-5"></v-img>
+                        </template>
                     </v-list-item>
                 </v-list>
             </v-navigation-drawer>
-            <v-app-bar >
+            <v-app-bar>
                 <v-app-bar-nav-icon @click="navigation.mini = !navigation.mini"
                     :icon="navigation.mini ? 'mdi-dots-vertical' : 'mdi-menu'">
                 </v-app-bar-nav-icon>
@@ -22,6 +28,7 @@
             <v-main>
                 <v-row>
                     <v-col class="pa-6">
+                        <v-img src=""></v-img>
                         <PythonPackages />
                     </v-col>
                 </v-row>
@@ -31,7 +38,6 @@
 </template>
 
 <script setup>
-import API from '@/assets/app/api';
 import { ref } from 'vue';
 
 
